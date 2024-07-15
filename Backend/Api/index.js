bfb295da7c866d62b681e3c6b164bd1de7b18201
Sushal-arch <sushalpokharel@gmail.com>
2024-07-15 16:20:11 +0545

    Connecting to the database

--- Backend/Api/index.js
+++ Backend/Api/index.js
@@ -1,5 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
+mongoose
+  /* eslint-disable no-undef */
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Database is connected");
+  })
+  .catch((err) => console.log(err));
 const app = express();
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
